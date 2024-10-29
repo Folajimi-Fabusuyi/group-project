5d04c05d95d01e60e8258483b11dede3716bbe0b
Folajimi Fabusuyi <folajimifabusuyi@gmail.com>
2024-10-29 22:31:44 +0000

    Add UCASE to problem1

--- problem1.py
+++ problem1.py
@@ -1,2 +1,12 @@
+'''
+Return the reverse of the input string
+Input: String -> "hello"
+Output: String -> "olleh"
+
+Edge Cases:
+    Input has to be a string for the function to work.
+    String can be empty
+'''
+
 def reverse_string(string):
     return string[::-1]
